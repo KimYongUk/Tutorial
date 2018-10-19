d3a9297f0c1c16744b5820e1406742996002df62
Kim YongUk <kimyonguk@ac919392.local>
2018-10-19 13:42:56 +0900

    커밋함

--- GitHubTutorial/GitHubTutorial/AppDelegate.swift
+++ GitHubTutorial/GitHubTutorial/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Xcode로 작성한 문구입니다.")
         return true
     }
 
