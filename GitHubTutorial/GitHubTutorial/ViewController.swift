89647d89e66b9fe69fd2ff5c5e40a15fcd10595c
Kim YongUk <kimyonguk@ac919392.local>
2018-10-23 09:53:40 +0900

    첫번째 커밋 with 푸시

--- GitHubTutorial/GitHubTutorial/ViewController.swift
+++ GitHubTutorial/GitHubTutorial/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("첫번째 클론에서 작성한 문구입니다.")
         print("첫번째 클론에서 작성한 문구입니다. 2018년 10월 23일")
     }
 
